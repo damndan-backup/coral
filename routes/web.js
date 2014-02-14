c2bfb262e271970eb2ac87b35222cdc4b276a1e0
vagrant <vagrant@precise32.(none)>
2014-02-14 09:39:31 +0000

    added functionality id and web parts

--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,9 @@
 var data = require('../webs.json');
 
 exports.view = function(req, res){
+	var web = req.params.webID
 	res.render('web',{
-		"webID": req.params.webID
-		"node" : data[req.params.webID]['posts']
+		"node": data[web]["posts"]
 	});
 };
 
